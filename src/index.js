d790aa999d2ed904045b201c7e2b53b22ec54b6c
Richard <richardzehungho@gmail.com>
2023-08-01 22:43:36 -0700

    adjusted weather and forecast for desktop view

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import getCity, { setFahrenheit, switched } from "./utility-functions";
 import weatherFetch from "./api-functions";
 
-// getCity();
-
 weatherFetch("Vancouver", "metric");
 setFahrenheit("Vancouver");
